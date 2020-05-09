b1e5024c0852a39bb02f8b56b07cfa2746a0c728
OtaxonMirzo <hasanzodaotaxonmirzo@gmail.com>
2020-05-10 03:15:11 +0500

    Asosiy oyna

---
+++ admin/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="bootstrap-3.3.7/css/bootstrap.min.css">
+
+</head>
+<body>
+<h3 align="center"> BOSH SAHIFA</h3>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Bosh sahifa</a></li>
+                            <li><a href="kategoriya.php">Kategoriya</a></li>
+                            <li><a href="Taomlar.php">Taomlar</a></li>
+                            <li><a href="portfolio.php">Portfoliya</a></li>
+                            <li><a href="messages.php">xabarlar</a></li>
+                            <li><a href="news.php">Yangliklar</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <form class="navbar-form navbar-left">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                            <i class="glyphicon glyphicon-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </ul>
+                    </div>
+                </div>
+        </nav>
+    </div>
+</div>
+</div>
+<h3 style="font-size: 55pt" align="center">  Admin panel</h3>
+</body>
